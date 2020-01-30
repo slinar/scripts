708a4fd91d6ce91e8810d0dc81c62a26aadff8a1
slinar <41759351+slinar@users.noreply.github.com>
2020-01-31 03:43:41 +0800

    use shellcheck (static analysis shell script)

--- openssh.sh
+++ openssh.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# Recently updated: 2020/1/11 21:43
+# Recently updated: 2020/1/31 3:30
+set -e
 
 openssl_ver="openssl-1.1.1d"
 openssh_ver="openssh-8.1p1"
@@ -12,25 +13,24 @@ install_zlib(){
         echo "zlib-1.2.11 already exists!"
         return
     fi
-    cd /tmp
+    cd /tmp || exit 1
     if [ ! -f zlib-1.2.11.tar.gz ];then
-        wget https://zlib.net/zlib-1.2.11.tar.gz
-        if [ $? -ne 0 ];then
+        if ! wget "https://zlib.net/zlib-1.2.11.tar.gz"; then
             rm -rf zlib-1.2.11.tar.gz
             echo "zlib-1.2.11.tar.gz download failed!"
             exit 1
         fi
     fi
-    tar xzf zlib-1.2.11.tar.gz
-    [ $? -ne 0 ] && echo "zlib-1.2.11.tar.gz Unpacking failed!" && exit 1
-    cd zlib-1.2.11
-    chmod +x configure
-    ./configure --prefix=/usr/local/zlib-1.2.11
-    [ $? -ne 0 ] && echo "Failed to configure zlib!" && exit 1
+    tar xzf zlib-1.2.11.tar.gz || exit 1
+    cd zlib-1.2.11 || exit 1
+    chmod 744 configure || exit 1
+    ./configure --prefix=/usr/local/zlib-1.2.11 \
+    || (echo "Failed to configure zlib!";exit 1)
     make
     make install
-    c=$( grep -x "/usr/local/zlib-1.2.11/lib" /etc/ld.so.conf|wc -l )
-    [ ${c} -eq 0 ] && sed -i '$a\/usr/local/zlib-1.2.11/lib' /etc/ld.so.conf
+    local count
+    count=$( grep -xc "/usr/local/zlib-1.2.11/lib" /etc/ld.so.conf )
+    [ "${count}" -eq 0 ] && sed -i '$a\/usr/local/zlib-1.2.11/lib' /etc/ld.so.conf
     ldconfig
 }
 
@@ -43,25 +43,24 @@ install_openssl(){
         echo "/etc/pki/tls/certs/ca-bundle.crt is not found!"
         exit 1
     fi
-    cd /tmp
+    cd /tmp || exit 1
     if [ ! -f ${openssl_ver}.tar.gz ];then
-        wget https://www.openssl.org/source/${openssl_ver}.tar.gz
-        if [ $? -ne 0 ];then
+        if ! wget "https://www.openssl.org/source/${openssl_ver}.tar.gz";then
             rm -rf ${openssl_ver}.tar.gz
             echo "${openssl_ver}.tar.gz download failed!"
             exit 1
         fi
     fi
-    tar xzf ${openssl_ver}.tar.gz
-    [ $? -ne 0 ] && echo "${openssl_ver}.tar.gz Unpacking failed!" && exit 1
-    cd ${openssl_ver}
-    chmod +x config
-    ./config --prefix=/usr/local/${openssl_ver} --openssldir=/usr/local/${openssl_ver}/ssl -fPIC
-    [ $? -ne 0 ] && echo "Failed to config openssl!" && exit 1
+    tar xzf ${openssl_ver}.tar.gz || exit 1
+    cd ${openssl_ver} || exit 1
+    chmod 744 config || exit 1
+    ./config --prefix=/usr/local/${openssl_ver} --openssldir=/usr/local/${openssl_ver}/ssl -fPIC \
+    || (echo "Failed to config openssl!";exit 1)
     make
     make install
-    c=$( grep -x "/usr/local/${openssl_ver}/lib" /etc/ld.so.conf|wc -l )
-    [ ${c} -eq 0 ] && sed -i '$a\/usr/local/'${openssl_ver}'/lib' /etc/ld.so.conf
+    local count
+    count=$( grep -xc "/usr/local/${openssl_ver}/lib" /etc/ld.so.conf )
+    [ "${count}" -eq 0 ] && sed -i '$a\/usr/local/'${openssl_ver}'/lib' /etc/ld.so.conf
     ldconfig
     rm -rf /usr/local/${openssl_ver}/ssl/certs
     ln -s /etc/pki/tls/certs /usr/local/${openssl_ver}/ssl/certs
@@ -69,11 +68,12 @@ install_openssl(){
 }
 
 modify_iptables(){
-    num=$( iptables -nvL|grep -E 'ACCEPT.*tcp.*dpt:'${sshd_port}''|grep -v grep|wc -l )
-    [ ${num} -eq 0 ] && num=$( iptables -nvL|grep -E 'ACCEPT.*tcp.*dports.* '${sshd_port}','|grep -v grep|wc -l )
-    [ ${num} -eq 0 ] && num=$( iptables -nvL|grep -E 'ACCEPT.*tcp.*dports.*,'${sshd_port}','|grep -v grep|wc -l )
-    [ ${num} -eq 0 ] && num=$( iptables -nvL|grep -E 'ACCEPT.*tcp.*dports.*,'${sshd_port}' '|grep -v grep|wc -l )
-    if [ ${num} -eq 0 ];then
+    local num
+    num=$( iptables -nvL|grep -E 'ACCEPT.*tcp.*dpt:'${sshd_port}''|grep -vc grep )
+    [ "${num}" -eq 0 ] && num=$( iptables -nvL|grep -E 'ACCEPT.*tcp.*dports.* '${sshd_port}','|grep -vc grep )
+    [ "${num}" -eq 0 ] && num=$( iptables -nvL|grep -E 'ACCEPT.*tcp.*dports.*,'${sshd_port}','|grep -vc grep )
+    [ "${num}" -eq 0 ] && num=$( iptables -nvL|grep -E 'ACCEPT.*tcp.*dports.*,'${sshd_port}' '|grep -vc grep )
+    if [ "${num}" -eq 0 ];then
         iptables -P INPUT DROP
         iptables -P FORWARD DROP
         iptables -P OUTPUT ACCEPT
@@ -85,15 +85,14 @@ modify_iptables(){
 
 privsep(){
     if [ ! -d /var/empty/sshd ];then
-        mkdir /var/empty/sshd
-        [ $? -ne 0 ] && echo "Create directory /var/empty/sshd failed" && exit 1
+        mkdir /var/empty/sshd || exit 1
     fi
     chown root:sys /var/empty/sshd
     chmod 755 /var/empty/sshd
-    gid=$( cat /etc/passwd|grep 'sshd:x:'|awk -F \: '{print $4}' )
+    gid=$( grep 'sshd:x:' /etc/passwd|awk -F : '{print $4}' )
     if [ -n "${gid}" ];then
-        gname=$( cat /etc/group|grep ${gid}|awk -F \: '{print $1}' )
-        [ ${gname} != "sshd" ] && echo "user:sshd does not belong to group:sshd!" && exit 1
+        gname=$( grep "${gid}" /etc/group|awk -F : '{print $1}' )
+        [ "${gname}" != "sshd" ] && echo "user:sshd does not belong to group:sshd!" && exit 1
     else
         groupadd sshd
         useradd -g sshd -c 'sshd privsep' -d /var/empty/sshd -s /sbin/nologin sshd
@@ -138,8 +137,10 @@ modify_selinux(){
 
 uninstall_old_openssh(){
     cp -f /etc/pam.d/sshd /etc/pam.d/sshd_bak >/dev/null 2>&1
+    set +e
     git --version >/dev/null 2>&1
     [ $? -eq 127 ] && yum -y remove openssh
+    set -e
     yum -y remove openssh-server
     chkconfig --del sshd
     rm -f /etc/ssh/moduli
@@ -151,36 +152,32 @@ uninstall_old_openssh(){
 download_openssh(){
     if [ ! -f ${openssh_ver}.tar.gz ];then
         wget https://cloudflare.cdn.openbsd.org/pub/OpenBSD/OpenSSH/portable/${openssh_ver}.tar.gz
-        if [ $? -ne 0 ];then
+        if ! wget "https://cloudflare.cdn.openbsd.org/pub/OpenBSD/OpenSSH/portable/${openssh_ver}.tar.gz";then
             rm -rf ${openssh_ver}.tar.gz
             echo "${openssh_ver}.tar.gz download failed!"
             exit 1
         fi
     fi
-    tar xzf ${openssh_ver}.tar.gz
-    [ $? -ne 0 ] && echo "${openssh_ver}.tar.gz Unpacking failed!" && exit 1
-    cd ${openssh_ver}
-    [ $? -ne 0 ] && echo "cd ${openssh_ver} failed!" && exit 1
-    chmod 744 configure
-    [ $? -ne 0 ] && exit 1
+    tar xzf ${openssh_ver}.tar.gz || exit 1
+    cd ${openssh_ver} || exit 1
+    chmod 744 configure || exit 1
 }
 
 install_openssh(){
-    cd /tmp
+    cd /tmp || exit 1
     download_openssh
     privsep
     if [ ${pam} == "0" ];then
-        ./configure --prefix=/usr --sysconfdir=/etc/ssh --with-ssl-dir=/usr/local/${openssl_ver} --with-zlib=/usr/local/zlib-1.2.11 --with-md5-passwords --with-privsep-path=/var/empty/sshd --with-privsep-user=sshd
-        [ $? -ne 0 ] && echo "Failed to configure openssh!" && exit 1
+        ./configure --prefix=/usr --sysconfdir=/etc/ssh --with-ssl-dir=/usr/local/${openssl_ver} --with-zlib=/usr/local/zlib-1.2.11 --with-md5-passwords --with-privsep-path=/var/empty/sshd --with-privsep-user=sshd \
+        || (echo "Failed to configure openssh!";exit 1)
     elif [ ${pam} == "1" ];then
-        ./configure --prefix=/usr --sysconfdir=/etc/ssh --with-ssl-dir=/usr/local/${openssl_ver} --with-zlib=/usr/local/zlib-1.2.11 --with-md5-passwords --with-pam --with-privsep-path=/var/empty/sshd --with-privsep-user=sshd
-        [ $? -ne 0 ] && echo "Failed to configure openssh!" && exit 1
+        ./configure --prefix=/usr --sysconfdir=/etc/ssh --with-ssl-dir=/usr/local/${openssl_ver} --with-zlib=/usr/local/zlib-1.2.11 --with-md5-passwords --with-pam --with-privsep-path=/var/empty/sshd --with-privsep-user=sshd \
+        || (echo "Failed to configure openssh!";exit 1)
     else
         echo 'pam value error! 0 or 1'
         exit 1
     fi
-    make
-    [ $? -ne 0 ] && echo "Failed to make openssh!" && exit 1
+    make || (echo "Failed to make openssh!";exit 1)
     trap "" 2
     uninstall_old_openssh
     make install
@@ -201,14 +198,15 @@ install_openssh(){
     chmod 600 /etc/ssh/ssh_host_dsa_key
     chkconfig --add sshd
     chkconfig sshd on
-    count=$( ps -ef|grep '/usr/sbin/sshd'|grep -v grep|wc -l )
-    if [ ${count} -eq 0 ];then
+    local sshd_pid
+    sshd_pid=$( pgrep -of '^/usr/sbin/sshd$' )
+    if [ -z "${sshd_pid}" ];then
         service sshd start
     else
         service sshd restart
     fi
-    count=$( ps -ef|grep '/usr/sbin/sshd'|grep -v grep|wc -l )
-    [ ${count} -eq 1 ] && echo "Successfully installed ${openssh_ver}!" && ssh -V
+    sshd_pid=$( pgrep -of '^/usr/sbin/sshd$' )
+    [ -n "${sshd_pid}" ] && echo "Successfully installed ${openssh_ver}!" && ssh -V
 }
 
 echo
