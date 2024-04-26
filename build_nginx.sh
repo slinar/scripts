2c54640d80769368b3d0b998b338048781bb4f3c
slinar <41759351+slinar@users.noreply.github.com>
2024-04-26 09:48:47 +0800

    nginx 1.26.x stable branch

--- build_nginx.sh
+++ build_nginx.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 libressl_ver="libressl-3.9.1"
 openssl_ver="openssl-3.0.13"
-nginx_ver="nginx-1.25.5"
+nginx_ver="nginx-1.26.0"
 fancyindex_ver="ngx-fancyindex-0.5.2"
 pcre2_ver="pcre2-10.43"
 zlib_ver="zlib-1.3.1"
