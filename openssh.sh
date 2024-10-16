d005dd461d2b42033114b127005d278c291abf1b
slinar <41759351+slinar@users.noreply.github.com>
2024-10-16 16:21:47 +0800

    update

--- openssh.sh
+++ openssh.sh
@@ -86,18 +86,6 @@ _download(){
 
 write_CentOS_Base(){
     cat > /etc/yum.repos.d/CentOS-Base.repo<<"EOF"
-# CentOS-Base.repo
-#
-# The mirror system uses the connecting IP address of the client and the
-# update status of each mirror to pick mirrors that are updated to and
-# geographically close to the client.  You should use this for CentOS updates
-# unless you are manually picking other mirrors.
-#
-# If the mirrorlist= does not work for you, as a fall back you can try the 
-# remarked out baseurl= line instead.
-#
-#
-
 [base]
 name=CentOS-$releasever - Base
 baseurl=http://mirrors.aliyun.com/centos-vault/6.10/os/$basearch/
